d1ea04bd4cc63bfb484241259fa5804149c7e9d7
Robin de Bruin <robin_de_bruin@live.nl>
2019-03-05 10:49:41 +0100

    added axios

--- src/components/Scores/Scores.jsx
+++ src/components/Scores/Scores.jsx
@@ -16,6 +16,7 @@ export default class Scores extends Component {
             highScoreList: [...prevState.highScoreList, e]
         }))
     }
+    
     render() {
         return (
             <div>
